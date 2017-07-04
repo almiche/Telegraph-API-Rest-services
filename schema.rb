925d6d9d9af000f22722d4c4ab2f854c05c61237
Michel Chatmajian <michelchatmajian@Michels-MacBook-Pro.local>
2017-07-04 06:51:52 -0400

    Schema modified to add to field

--- schema.rb
+++ schema.rb
@@ -3,29 +3,11 @@ require 'mongoid'
 
 Mongoid.load!('mongoid.yml', :production)
 
-# class MorseUsers
-#   include Mongoid::Document
-  
-#   embeds_one:correspondance, :class_name => "Correspondance"
-  
-#   field :users, type:Array
-#   store_in collection: 'morse_code',database:' mike'
-# end
-
-# class Correspondance
-#     include Mongoid::Document
-
-#   field :date, type: Date
-#   field :decodedmessage, type: String
-#   field :codedmessage, type: String
-
-#   embedded_in :morseusers
-# end
-
 class Correspondance
     include Mongoid::Document
 
   field :user_name,type: String
+  field :to,type: String
   field :date, type: Date
   field :decodedmessage, type: String
   field :codedmessage, type: String
