73e656f0242990d079af6deb3a3422208a771466
almiche <chamich196@hotmail.com>
2017-07-04 06:53:52 -0400

    /conversation route now functional returns raw json of conversations

--- index.rb
+++ index.rb
@@ -7,25 +7,26 @@ get '/send' do
     user = params['user']
     morse_message = params['message']
     mes = decode(morse_message)
+    reciever = params['to']
     puts "#{mes}"
-    new_message(user,decode(morse_message),morse_message)
+    new_message(user,reciever,decode(morse_message),morse_message)
    
 end    
 
 #Route to recieve telegrams
 get '/conversation' do
     #We want to return the correspondance between these to users
-    users = params['users'].split(":")
-
-     "The follwoing users were in this Correspondance #{users}"
+    current_user=params['user']
+    Correspondance.collection.find({ user_name: current_user }).select.to_json
 
 end
 
 
-def new_message(user_name,decoded_message,coded_message)
+def new_message(user_name,to,decoded_message,coded_message)
 Correspondance.create(
 
     user_name:user_name,
+    to:to,
     date:DateTime.now,
     decodedmessage:decoded_message,
     codedmessage:coded_message
