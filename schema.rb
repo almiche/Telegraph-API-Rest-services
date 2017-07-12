892aaff677340eac03bfd165c63a7010894af3b4
almiche <chamich196@hotmail.com>
2017-07-11 22:37:54 -0400

    Modify Correspondance schema to contain key value pairs of messages

--- schema.rb
+++ schema.rb
@@ -3,14 +3,17 @@ require 'mongoid'
 
 Mongoid.load!('mongoid.yml', :production)
 
+#Both of the message fields will be encrypted using pgp
+#messages are hashes of key values pairs 
+#key will denote the person who can decrypt message
 class Correspondance
     include Mongoid::Document
 
   field :user_name,type: String
   field :to,type: String
   field :date, type: Date
-  field :decodedmessage, type: String
-  field :codedmessage, type: String
+  field :decodedmessage, type: Hash
+  field :codedmessage, type: Hash
 
 end
 
@@ -20,7 +23,7 @@ class User
 
   field :user_name,type: String
   field :public_key,type: String
-  field :conversation_ids,type: Array
+  field :correspondance_ids,type: Array
   field :password_hash,type: String
   field :password_salt,type: String
 
